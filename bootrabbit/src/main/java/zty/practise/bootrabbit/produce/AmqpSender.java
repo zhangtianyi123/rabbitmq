cbc8c69b57f1f266834c5299fadae3ec7420b3b4
zhangty <465355874@qq.com>
2019-09-28 20:33:44 +0800

    定时生产消息

--- bootrabbit/src/main/java/zty/practise/bootrabbit/produce/AmqpSender.java
+++ bootrabbit/src/main/java/zty/practise/bootrabbit/produce/AmqpSender.java
@@ -4,10 +4,13 @@ package zty.practise.bootrabbit.produce;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service("amqpSender")
+@Slf4j
 public class AmqpSender {
 	
     @Autowired
@@ -19,7 +22,11 @@ public class AmqpSender {
 	@Value("${rabbitmq.requestRoutingkey}")
 	private String routingKey;
 	
-    public void send(String message) {
-        this.rabbitTemplate.convertAndSend(exchangeName, routingKey, message);
+	private static long count = 0;
+	
+	@Scheduled(fixedRate = 3000)
+    public void send() {
+        this.rabbitTemplate.convertAndSend(exchangeName, routingKey, (count++) + "");
+        log.info("send message...");
     }
 }
